cb4791ef2a9dd1a872187f4cca6f63a618aa0561
David Sykora <dsykora@gmail.com>
2013-07-14 11:06:37 -0700

    fix

--- server/methods.js
+++ server/methods.js
@@ -4,6 +4,12 @@ Meteor.startup(function () {
       Destinations.find({'serviceStatus.state': "pending"}).forEach(function(Destination){
         Destinations.update(Destination._id, {$set:{'serviceStatus.state': "idle", 'serviceStatus.responderId':null}});
       });
+      Destinations.find({'serviceStatus.state': "responding"}).forEach(function(Destination){
+        Destinations.update(Destination._id, {$set:{'serviceStatus.state': "idle", 'serviceStatus.responderId':null}});
+      });
+      Destinations.find({'serviceStatus.state': "busy"}).forEach(function(Destination){
+        Destinations.update(Destination._id, {$set:{'serviceStatus.state': "idle", 'serviceStatus.responderId':null}});
+      });
     }
   });
 });
