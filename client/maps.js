0e6b243fc867f970675d91445aef4b64dbec5e88
Benjamin Morse <bjmorse@gmail.com>
2013-07-13 20:19:41 -0700

    * trying to add callouts to pins, but all the same

--- client/maps.js
+++ client/maps.js
@@ -2,8 +2,8 @@ incarMapObject = null;
 
 initializeCarMap = function() {
     var mapOptions = {
-        center: new google.maps.LatLng(37.397, -120.644),
-        zoom: 8,
+        center: new google.maps.LatLng(37.418909, -122.093725),
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     incarMapObject = new google.maps.Map(document.getElementById("car-map-canvas"), mapOptions);
@@ -29,19 +29,27 @@ populateCarMapPins = function()
     {
         var pin = pins[x];
 
+        // callout window (content can be full html)
+        var infowindow = new google.maps.InfoWindow({
+            content: "" + pin.lat + "," + pin.lon
+        });
+
         var pinLatlng = new google.maps.LatLng(pin.lat, pin.lon);
         var marker = new google.maps.Marker({
             position: pinLatlng,
             title:""
         });
 
+        // wire click for pin
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(incarMapObject,marker);
+        });
+
         // To add the marker to the map, call setMap();
         marker.setMap(incarMapObject);
 
+        (function(){});
     }
 
 //    console.log(d);
-
-
-
 }
