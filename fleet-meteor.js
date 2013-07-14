672e3921bcf91548b3a3822f76d03f477bf3c864
David Sykora <dsykora@gmail.com>
2013-07-14 05:39:56 -0700

    Cars look for assigned cases

--- fleet-meteor.js
+++ fleet-meteor.js
@@ -8,10 +8,9 @@ if (Meteor.isClient) {
     }
   };
 
-  Template.sideMenu.created = function () {
-//    Meteor.setInterval(function(){
-//
-//    }, 200);
+
+  Template.sideMenu.rendered = function () {
+    watchForServiceEvents();
   };
 
   Template.sideMenu.events({
@@ -68,10 +67,18 @@ if (Meteor.isClient) {
 
   Template.login.events({
     'click #carOne' : function () {
-      Meteor.loginWithPassword('carOne', 'pass');
+      Meteor.loginWithPassword('carOne', 'pass', function(error){
+        if(!error){
+          watchForServiceEvents();
+        }
+      });
     },
     'click #carTwo' : function () {
-      Meteor.loginWithPassword('carTwo', 'pass');
+      Meteor.loginWithPassword('carTwo', 'pass', function(error){
+        if(!error){
+          watchForServiceEvents();
+        }
+      });
     }
   });
 }
@@ -82,5 +89,52 @@ if (Meteor.isServer) {
 
     // call calculate now, this loops every n period
     calculateLoop();
+
+    //look for events, update id of responder
   });
 }
+
+//Look for pending events with my id, launch modal
+var watchForServiceEvents = function(){
+  Destinations.find({'serviceStatus.state':"pending", 'serviceStatus.responderId':Meteor.userId()}).observe({
+    added: function(document){
+      //New Situation
+      bootbox.dialog("A situation is underway, are you ready?", [{
+        "label" : "Yes, I'm on the case!",
+        "class" : "btn-success",
+        "callback": function() {
+          var mp3Url = Session.get('baseUrl') + '/preview/Fleet/media/LetsRock.mp3';
+          audioHandle = gm.media.play(mp3Url, 'mixedAudio');
+
+          Meteor.setTimeout(function(){
+            var mp3Url = Session.get('baseUrl') + '/preview/Fleet/media/Knight%20Rider%20Theme%20Song%20Bass.mp3';
+            audioHandle2 = gm.media.play(mp3Url, 'mixedAudio');
+          }, 3000);
+
+          var options = {latitude:(document.lat / 0.000000277777777778), longitude:(document.lon / 0.000000277777777778)};
+
+          console.log(options);
+          //Set destination for NAV
+          gm.nav.setDestination(function(){}, function(){}, options);
+
+          //Update DB status, on my way...
+          Destinations.update(document._id, {$set:{'serviceStatus.state': "responding"}});
+          Session.set('onMyWay', true);
+        }
+      }, {
+        "label" : "Nope, can't make it.",
+        "class" : "btn-danger",
+        "callback": function() {
+          var mp3Url = Session.get('baseUrl') + '/preview/Fleet/media/QuitWastingMyTime.mp3';
+          audioHandle = gm.media.play(mp3Url, 'mixedAudio');
+
+          //Tell DB I'm not coming
+          Destinations.update(document._id, {$set:{'serviceStatus.state': "busy"}});
+          Session.set('onMyWay', false);
+        }
+      }]);
+
+    }
+  });
+
+};
